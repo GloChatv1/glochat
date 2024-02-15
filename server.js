b610c6889c5c20042130ad636b4e297d88c848b0
unknown <rickrolletwas@gmail.com>
2024-02-15 16:53:57 +0100

    random usernames added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
+const { generateUsername } = require("unique-username-generator");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -20,39 +22,53 @@ app.use((req, res) => {
 
 let messages = [];
 
-io.on('connection', (socket) => {
-    // Assign a unique ID to the user and store it in the users object
-    const userId = socket.id;
-    users[userId] = userId;
+// Mapping of user IDs to display names
+let userDisplayNames = {};
 
-    console.log(`${new Date().toLocaleTimeString()} connect ${userId}`);
+io.on('connection', (socket) => {
+    // Get the IP address of the user
+    const userIP = socket.handshake.address;
+
+    // Check if the user has previously connected and has a display name
+    let displayName = userDisplayNames[userIP];
+    if (!displayName) {
+        // If not, generate a new display name for the user
+        displayName = getDisplayName();
+        // Store the display name for the user's IP address
+        userDisplayNames[userIP] = displayName;
+    }
+    
+    console.log(`${new Date().toLocaleTimeString()} connect ${userDisplayNames[userIP]}`);
     
     // Emit 'user connected' event when a user connects
-    io.emit('chat message', `<span class="user-connect">${userId} has connected</span>`);
-
+    io.emit('chat message', `<span class="user-connect">${userDisplayNames[userIP]} has connected</span>`);
+    
     // Send chat history to the newly connected user
     socket.emit('chat history', messages);
 
     socket.on('chat message', (msg) => {
         const timestamp = new Date().toLocaleTimeString(); // Get current time
+        const displayName = userDisplayNames[userIP]; // Get the display name for the user ID
         const messageWithTimestamp = `<div class="message">
-    <div class="user-info">
-        <div class="user-id">${userId}</div>
-        <span class="timestamp">${timestamp}</span>
-    </div>
-    <div class="message-content">
-        <span class="message-text">${msg}</span>
-    </div>
-</div>`;
+            <div class="user-info">
+                <div class="user-id">${displayName}</div>
+                <span class="timestamp">${timestamp}</span>
+            </div>
+            <div class="message-content">
+                <span class="message-text">${msg}</span>
+            </div>
+        </div>`;
 
         messages.push(messageWithTimestamp);
         io.emit('chat message', messageWithTimestamp);
-        console.log(`${timestamp} message ${userId} : \"${msg}\"`);
+        console.log(`${timestamp} message ${displayName} : \"${msg}\"`);
     });
+    
 
     // Handle sending images
     socket.on('chat image', (dataUrl) => {
         const timestamp = new Date().toLocaleTimeString(); // Get current time
+        const displayName = userDisplayNames[userIP];
         const imageMessageWithTimestamp = `<div class="message">
     <div class="user-info">
         <div class="user-id">${userId}</div>
@@ -64,17 +80,21 @@ io.on('connection', (socket) => {
 </div>`;
         messages.push(imageMessageWithTimestamp);
         io.emit('chat message', imageMessageWithTimestamp);
-        console.log(`${timestamp} image ${userId}`);
+        console.log(`${timestamp} image ${displayName}`);
     });
 
     socket.on('disconnect', () => {
-        console.log(`${new Date().toLocaleTimeString()} disconnect ${userId}`);
+        const displayName = userDisplayNames[userIP];
+        console.log(`${new Date().toLocaleTimeString()} disconnect ${displayName}`);
         // Remove user from users object on disconnect
-        delete users[userId];
+        delete users[userIP];
         // Emit 'user disconnected' event when a user disconnects
-        io.emit('chat message', `<span class="user-disconnect">${userId} has disconnected</span>`);
+        io.emit('chat message', `<span class="user-disconnect">${displayName} has disconnected</span>`);
     });
 });
 
-
 server.listen(3000, () => console.log('Listening on port 3000'));
+
+function getDisplayName(){
+    return generateUsername("-");
+}
